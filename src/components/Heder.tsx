49593ee88fcbafd1445106d9001b3d72fd639dde
na-613c <na613c613c@gmail.com>
2021-06-06 03:36:35 +0300

    [ADD] active tab

--- src/components/Heder.tsx
+++ src/components/Heder.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout, Menu } from 'antd';
 import { PATH_MAIN_PAGE, PATH_CREATE_POST_PAGE } from '../path'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 
 const { Header } = Layout;
 
 
 const HeaderApp = () => {
+
+    const [selectedKeys, setSelectedKeys] = useState(['2'])
+
+    let location = useLocation()
+
+    useEffect(() => {
+        switch (location.pathname) {
+            case PATH_MAIN_PAGE:
+                setSelectedKeys(['1']);
+                break;
+            case PATH_CREATE_POST_PAGE:
+                setSelectedKeys(['2']);
+                break;
+            default: setSelectedKeys(['0']);
+        }
+    }, [location])
+
     return (
         <Header style={{ marginBottom: 50 }}>
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+            <Menu theme="dark" mode="horizontal" selectedKeys={selectedKeys}>
                 <Menu.Item key="1">
                     <NavLink to={PATH_MAIN_PAGE}>Main page</NavLink>
                 </Menu.Item>
